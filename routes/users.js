a531ffd4b0c4f8007b6fffe6fbeb83d79dba6f06
AndreiMattos <andreimattos06@yahoo.com.br>
2018-08-13 05:03:30 -0400

    Commit

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('index', { title: 'Express' });
+});
+
+/* SET home page. */
+router.post('/', function(req, res, next) {
+  res.redirect('/login');
+});
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login' });
+});
+
+/* SET login page. */
+router.post('/login', function(req, res, next) {
+  var nome = req.body.nome;
+  var idade = req.body.idade;
+  res.redirect('/?nome=' + nome);
 });
 
 module.exports = router;
