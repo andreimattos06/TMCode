80441cb6e21def10d971b25558b6da052d1a4468
AndreiMattos <andreimattos06@yahoo.com.br>
2018-08-13 04:54:53 -0400

    Login

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,21 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* SET home page. */
+router.post('/', function(req, res, next) {
+  res.redirect('/login');
+});
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login' });
+});
+
+/* SET login page. */
+router.post('/login', function(req, res, next) {
+  var nome = req.body.nome;
+  var idade = req.body.idade;
+  res.redirect('/?nome=' + nome);
+});
+
 module.exports = router;
